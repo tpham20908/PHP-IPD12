7350d9e037e0029ba6a5c365016fff7507fe1cb2
unknown <tpham20908@gmail.com>
2018-04-15 09:14:34 -0400

    first commit

---
+++ PhpProject1/index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+
+        function longdate($timestamp) {
+          return date("l F jS Y", $timestamp);
+        }
+        
+        ?>
+    </body>
+</html>
