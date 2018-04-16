6b475fbaaf4021a6ad9f081a967ed408e90f5a63
unknown <tpham20908@gmail.com>
2018-04-16 16:17:23 -0400

    create folder, files neccessary for SharePosts project

---
+++ SharePostsMVC/public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>SharePosts Project</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
