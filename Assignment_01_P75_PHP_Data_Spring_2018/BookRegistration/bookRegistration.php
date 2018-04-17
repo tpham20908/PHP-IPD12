5e281a0710c31c8995981c39ce0cecf63b4cf126
unknown <tpham20908@gmail.com>
2018-04-17 18:06:35 -0400

    designing page validinput in BookRegister project

--- Assignment_01_P75_PHP_Data_Spring_2018/BookRegistration/bookRegistration.php
+++ Assignment_01_P75_PHP_Data_Spring_2018/BookRegistration/bookRegistration.php
@@ -5,3 +5,46 @@
  * PHP Assignment1 
  */
 
+$firstName = $_POST["firstName"];
+$lastName = $_POST["lastName"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+$book = $_POST["book"];
+$operating_system = $_POST["os"];
+
+$phone_pattern = "/^\([0-9]{3}\)[0-9]{3}-[0-9]{4}$/";
+
+$invalid_msg = <<<EOD
+        Invalid phone number<br>
+        A valid phone number must be in the form (555)555-5555<br>
+        Click the Back button, enter a valid phone number and resubmit<br><br>
+        
+        Thank you.<br>
+EOD;
+
+$valid_msg = <<<EOD
+        Hi $firstName. Thank you for completing the survey.<br>
+        You have been added to the $book mailing list.<br>
+        <br>
+        <strong>The following information has been saved in our database:</strong><br><br>
+        <table>
+            <thead>
+                <tr>
+                    <td>Name</td>
+                    <td>Email</td>
+                    <td>Phone</td>
+                    <td>OS</td>
+                </tr>
+            </thead>
+        </table>
+        
+EOD;
+
+if (preg_match($phone_pattern, $phone) != 1)
+{
+    echo $invalid_msg;
+}
+else 
+{
+    echo $valid_msg;
+}
--- Assignment_01_P75_PHP_Data_Spring_2018/BookRegistration/myscripts.js
+++
@@ -1,9 +0,0 @@
-var firstName = document.getElementById("firstName");
-
-firstName.addEventListener("input", function (event) {
-    if (firstName.validity.valueMissing) {
-        firstName.setCustomValidity("This is a required field.");
-    } else {
-        firstName.setCustomValidity("");
-    }
-});
