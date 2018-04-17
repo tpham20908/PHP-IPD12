cf2b0ce467f8ad57bed107431ae7c55d5a05ba1f
unknown <tpham20908@gmail.com>
2018-04-17 19:31:55 -0400

    finish BookRegister assignment v1

--- Assignment_01_P75_PHP_Data_Spring_2018/BookRegistration/bookRegistration.php
+++ Assignment_01_P75_PHP_Data_Spring_2018/BookRegistration/bookRegistration.php
@@ -1,30 +1,38 @@
-<?php
-/* 
- * bookGegistration.php
- * Tung Pham IPD12 - 2018
- * PHP Assignment1 
- */
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Book Registration</title>
+        <link rel="stylesheet" href="style.css"/>
+    </head>
+    <body>
+        <?php
+        /*
+         * bookGegistration.php
+         * Tung Pham IPD12 - 2018
+         * PHP Assignment1 
+         */
 
-$firstName = $_POST["firstName"];
-$lastName = $_POST["lastName"];
-$email = $_POST["email"];
-$phone = $_POST["phone"];
-$book = $_POST["book"];
-$operating_system = $_POST["os"];
+        $firstName = $_POST["firstName"];
+        $lastName = $_POST["lastName"];
+        $email = $_POST["email"];
+        $phone = $_POST["phone"];
+        $book = ucwords($_POST["book"]);
+        $operating_system = $_POST["os"];
 
-$phone_pattern = "/^\([0-9]{3}\)[0-9]{3}-[0-9]{4}$/";
+        $phone_pattern = "/^\([0-9]{3}\)[0-9]{3}-[0-9]{4}$/";
 
-$invalid_msg = <<<EOD
-        Invalid phone number<br>
-        A valid phone number must be in the form (555)555-5555<br>
-        Click the Back button, enter a valid phone number and resubmit<br><br>
+        $invalid_msg = <<<EOD
+        <span class="red">Invalid phone number</span><br>
+        A valid phone number must be in the form <strong>(555)555-5555</strong><br>
+        <span class="blue">Click the Back button, enter a valid phone number and resubmit</span><br><br>
         
-        Thank you.<br>
+        <span class="blue">Thank you.</span><br>
 EOD;
 
-$valid_msg = <<<EOD
-        Hi $firstName. Thank you for completing the survey.<br>
-        You have been added to the $book mailing list.<br>
+        $valid_msg = <<<EOD
+        Hi <span class="blue">$firstName</span>. Thank you for completing the survey.<br>
+        You have been added to the <span class="blue">$book</span> mailing list.<br>
         <br>
         <strong>The following information has been saved in our database:</strong><br><br>
         <table>
@@ -36,15 +44,24 @@ $valid_msg = <<<EOD
                     <td>OS</td>
                 </tr>
             </thead>
+            <tbody>
+                <tr>
+                    <td>$firstName $lastName</td>
+                    <td>$email</td>
+                    <td>$phone</td>
+                    <td>$operating_system</td>
+                </tr>
+            </tbody>
         </table>
         
 EOD;
 
-if (preg_match($phone_pattern, $phone) != 1)
-{
-    echo $invalid_msg;
-}
-else 
-{
-    echo $valid_msg;
-}
+        if (preg_match($phone_pattern, $phone) != 1) {
+            echo $invalid_msg;
+        } else {
+            echo $valid_msg;
+        }
+        ?>
+    </body>
+
+        
