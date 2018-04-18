da0b950a17884b449b9087845d44be3b25ae3cad
unknown <tpham20908@gmail.com>
2018-04-17 20:52:33 -0400

    SharePosts: create Pages.php

--- SharePostsMVC/app/libraries/Core.php
+++ SharePostsMVC/app/libraries/Core.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * App Core Class
  * Creates URL & loads core controller
@@ -6,20 +7,36 @@
  */
 
 class Core {
+
     protected $currentController = "Pages";
     protected $currentMethod = "index";
     protected $params = [];
-    
+
     public function __construct() {
-        print_r($this->getUrl());
+        $url = $this->getUrl();
+
+        // Look in controllers for first value
+        if (file_exists("../app/controllers/" . ucwords($url[0]) . ".php")) {
+            // If exists, set as controller
+            $this->currentController = ucwords($url[0]);
+            // Unset 0 index
+            unset($url[0]);
+        }
+        
+        // Require the controller
+        require_once "../app/controllers/" . $this->currentController . ".php";
+        
+        // Instantiate controller class
+        $this->currentController = new $this->currentController;
     }
-    
+
     protected function getUrl() {
         if (isset($_GET["url"])) {
             $url = rtrim($_GET["url"], "/");
             $url = filter_var($url, FILTER_SANITIZE_URL);
             $url = explode("/", $url);
+            return $url;
         }
-        return $url;
     }
+
 }
