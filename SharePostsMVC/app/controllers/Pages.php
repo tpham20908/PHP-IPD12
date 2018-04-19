6b87dff8d8e3e6ad8a9ab618e7bfa9bc9dae7c18
unknown <tpham20908@gmail.com>
2018-04-19 10:51:40 -0400

    create Controller class and Pages extends Controller

--- SharePostsMVC/app/controllers/Pages.php
+++ SharePostsMVC/app/controllers/Pages.php
@@ -1,13 +1,13 @@
 <?php
 
-class Pages {
+class Pages extends Controller {
 
     public function __construct() {
         
     }
 
     public function index() {
-        
+        $this->view("Hello");
     }
 
     public function about($id) {
---
+++ SharePostsMVC/app/libraries/Controller.php
@@ -0,0 +1,25 @@
+<?php
+
+class Controller {
+    // Load model
+    public function model($model) {
+        // Require model file
+        require_once "../app/models/" . $model . ".php";
+        
+        // Instantiate model
+        return new $model();
+    }
+    
+    // Load view
+    public function view($view, $data = []) {
+        // Check for view file
+        if (file_exists("../app/views/" . $view . ".php")) {
+            // Require view
+            require_once "../app/views/" . $view . ".php";
+        }
+        else {
+            // View does not exist
+            die ("View does not exist.");
+        }
+    }
+}
