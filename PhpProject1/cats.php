28bb1de0c26401a2e983991d1ebfe8396a768b89
unknown <tpham20908@gmail.com>
2018-04-18 21:08:07 -0400

    cats.php describe table cats in publications db

--- PhpProject1/cats.php
+++ PhpProject1/cats.php
@@ -14,8 +14,29 @@ $query = <<<_END
     )
 _END;
 */
+/*
+$query = "DESCRIBE cats";
 
+$result = $conn->query($query);
+if (!$result) die ("Database access failed: " . $conn->error);
 
+$rows = $result->num_rows;
+
+echo "<table><tr><th>Column</th><th>Type</th><th>Null</th><th>Key</th><th>Default</th><th>Extra</th></tr>";
+
+for ($i = 0; $i < $rows; $i++) {
+    $result->data_seek($i);
+    $row = $result->fetch_array(MYSQLI_NUM);
+    echo "<tr>";
+    for ($j = 0; $j < 6; $j++) {
+        echo "<td>$row[$j]</td>";
+    }
+    echo "</tr>";
+}
+echo "</table>";
+*/
+
+$query = "DESCRIBE cats";
 
 $result = $conn->query($query);
 if (!$result) die ("Database access failed: " . $conn->error);
