7a079ff8b4d96d117385a77126b51e6852fb0c9b
unknown <tpham20908@gmail.com>
2018-04-20 14:13:56 -0400

    create Database constants in config.php

--- SharePostsMVC/app/config/config.php
+++ SharePostsMVC/app/config/config.php
@@ -1,4 +1,17 @@
 <?php
+// DB Params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "pdoposts");
+
+/*
+define("DB_HOST", "den1.mysql4.gear.host");
+define("DB_USER", "jac18");
+define("DB_PASS", "tp%ipd12");
+define("DB_NAME", "jac18");
+ */
+
 // App root
 define("APPROOT", dirname(dirname(__FILE__)));
 // URL root
