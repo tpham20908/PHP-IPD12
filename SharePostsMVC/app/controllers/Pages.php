a0592d8b4264fd95c4d7e1db938079f61bc82a9b
unknown <tpham20908@gmail.com>
2018-04-20 21:03:42 -0400

    add bootstrap links for css and javascript and fontawesome

--- SharePostsMVC/app/controllers/Pages.php
+++ SharePostsMVC/app/controllers/Pages.php
@@ -3,13 +3,15 @@
 class Pages extends Controller {
 
     public function __construct() {
-        $this->postModel = $this->model("Post");
+        
     }
 
     public function index() {
         $data = [
-          "title" => "Welcome"
+            "title" => "Share Posts"
         ];
+        
+        
         $this->view("pages/index", $data);
     }
 
--- SharePostsMVC/app/models/Post.php
+++ SharePostsMVC/app/models/Post.php
@@ -5,4 +5,12 @@ class Post {
     public function __construct() {
         $this->db = new Database;
     }
+    
+    public function getPosts() {
+        $sql = "SELECT * FROM posts;";
+        $this->db->query($sql);
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
 }
