afc10b12fcfffd2e3b1d476a1cd8860e8cda0eff
unknown <tpham20908@gmail.com>
2018-04-21 23:06:38 -0400

    require session_helper in bootstrap and plug in flash() method in login and Controllers/User

--- SharePostsMVC/app/helpers/session_helper.php
+++ SharePostsMVC/app/helpers/session_helper.php
@@ -13,11 +13,11 @@ function flash($name = "", $message = "", $class = "alert alert-success") {
                 unset($_SESSION[$name . "_class"]);
             }
             
-            $_SESSION[$name] = $name;
+            $_SESSION[$name] = $message;
             $_SESSION[$name . "_class"] = $class;
         } elseif (empty($message) && !empty($_SESSION[$name])) {
-            $class = !empty($_SESSION[$name . "_class"]) ? !empty($_SESSION[$name . "_class"]) : "";
-            echo "<div class=\"$class\" id=\"msg-flash\">$_SESSION[$message]</div>";
+            $class = !empty($_SESSION[$name . "_class"]) ? $_SESSION[$name . "_class"] : "";
+            echo "<div class=\"$class\" id=\"msg-flash\">$_SESSION[$name]</div>";
             unset($_SESSION[$name]);
             unset($_SESSION[$name . "_class"]);
         }
