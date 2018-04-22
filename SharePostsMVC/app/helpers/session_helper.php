469266f5ce60d675140880aa84b28720727bd03b
unknown <tpham20908@gmail.com>
2018-04-21 22:49:13 -0400

    create session_helper.php

---
+++ SharePostsMVC/app/helpers/session_helper.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+// Flash message helper
+function flash($name = "", $message = "", $class = "alert alert-success") {
+    if (!empty($name)) {
+        if (!empty($message) && empty($_SESSION[$name])) {
+            if (!empty($_SESSION[$name])) {
+                unset($_SESSION[$name]);
+            }
+            
+            if (!empty($_SESSION[$name . "_class"])) {
+                unset($_SESSION[$name . "_class"]);
+            }
+            
+            $_SESSION[$name] = $name;
+            $_SESSION[$name . "_class"] = $class;
+        } elseif (empty($message) && !empty($_SESSION[$name])) {
+            $class = !empty($_SESSION[$name . "_class"]) ? !empty($_SESSION[$name . "_class"]) : "";
+            echo "<div class=\"$class\" id=\"msg-flash\">$_SESSION[$message]</div>";
+            unset($_SESSION[$name]);
+            unset($_SESSION[$name . "_class"]);
+        }
+    }
+}
