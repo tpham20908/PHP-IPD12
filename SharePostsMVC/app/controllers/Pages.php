bc6f50c9a51e7987d7d418b302cae1409fb4d4af
unknown <tpham20908@gmail.com>
2018-04-22 22:31:18 -0400

    create add() in Controller/Posts and views/posts/add.php

---
+++ SharePostsMVC/app/controllers/Pages.php
@@ -0,0 +1,29 @@
+<?php
+
+class Pages extends Controller {
+
+    public function __construct() {
+        
+    }
+
+    public function index() {
+        if (isLoggedIn())
+            redirect("posts");
+        
+        $data = [
+            "title" => "Share Posts",
+            "description" => "Social network build on the PHP MVC and Bootstrap Framework"
+        ];
+        
+        
+        $this->view("pages/index", $data);
+    }
+
+    public function about() {
+        $data = [
+            "title" => "About us",
+            "description" => "App to share posts with other users"
+        ];
+        $this->view("pages/about", $data);
+    }
+}
