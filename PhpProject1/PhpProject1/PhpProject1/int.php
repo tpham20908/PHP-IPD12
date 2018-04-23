a4f7dcce37d71a5d5079f21bfe476024edfa8e11
Tung Pham <tungint@gmail.com>
2018-04-23 11:56:17 -0400

    day7 in class

--- PhpProject1/PhpProject1/PhpProject1/int.php
+++ PhpProject1/PhpProject1/PhpProject1/int.php
@@ -1,18 +1,5 @@
 <?php
-
-/* 
- * init.php
- * initialization
- * @version 1.2 2018-04-19
- * @package Smithside Auctions
- * @copyright (c) 2018, Smithside Auctions
- * @license GNU General Public License
- * @since Release 1.0
- * @author Tung Pham
- */
-
 function __autoload($class_name) {
-    // require 'includes/classes/' . strtolower($class_name) . '.php';
     try {
         $class_file = "includes/classes/" . strtolower($class_name) . ".php";
         if (is_file($class_file)) {
@@ -25,4 +12,3 @@ function __autoload($class_name) {
     }
 }
 
-require_once 'includes/function.php';
